21c9692dd5376c7e964451e05c1dd649a7726f98
Lynerd <tilleryl10@gmail.com>
2024-01-16 17:19:18 -0500

    getting get request error

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,34 @@
-const express = require('express')
+const express = require("express");
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
 
-const app = express()
-const cors = require('cors')
-const config = { port: process.env.PORT || 3000 }
+const lorem = new LoremIpsum({
+  sentencesInParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsInSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+const app = express();
+const cors = require("cors");
+
+const config = { port: process.env.PORT || 3000 };
 
 // middleware
-app.use(cors())
+app.use(cors());
 
 // your API route(s) here
+app.get("/lorem", (req, res) => {
+  res.json({ lorem: lorem.generateSentences(8) });
+});
 
-
-app.get('*', function (req, res) {
-  res.status(404).json({ error: 'route not found' })
-})
+app.get("*", function (req, res) {
+  res.status(404).json({ error: "route not found" });
+});
 
 // start server
 app.listen(config.port, () => {
-  console.log(`App listening at http://localhost:${config.port}`)
-})
+  console.log(`App listening at http://localhost:${config.port}`);
+});
